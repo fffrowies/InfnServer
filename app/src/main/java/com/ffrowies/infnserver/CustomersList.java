5cd4b7266f404013e6bbbb487c01423fff522066
fffrowies <fernandorowies.saxo@gmail.com>
2019-02-07 22:52:17 -0300

    Search customers

--- app/src/main/java/com/ffrowies/infnserver/CustomersList.java
+++ app/src/main/java/com/ffrowies/infnserver/CustomersList.java
@@ -1,11 +1,14 @@
 package com.ffrowies.infnserver;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,8 +16,15 @@ import com.ffrowies.infnserver.Interface.ItemClickListener;
 import com.ffrowies.infnserver.Models.User;
 import com.ffrowies.infnserver.ViewHolder.CustomerViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomersList extends AppCompatActivity {
 
@@ -25,6 +35,11 @@ public class CustomersList extends AppCompatActivity {
     DatabaseReference customersList;
 
     FirebaseRecyclerAdapter<User, CustomerViewHolder> adapter;
+
+    //Search functionality
+    FirebaseRecyclerAdapter<User, CustomerViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +65,102 @@ public class CustomersList extends AppCompatActivity {
         });
 
         loadCustomersList();
+
+        //Search
+        materialSearchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Enter customer name");
+        loadSuggest();
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //To change suggest list while user type text
+                List<String> suggest = new ArrayList<String>();
+                for (String search:suggestList)
+                {
+                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                //When Search Bar is closed
+                //restore original adapter
+                if (!enabled)
+                    recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                //When search finish
+                // show result of search
+                startSearch(text);
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+    }
+
+    private void startSearch(CharSequence text) {
+        searchAdapter = new FirebaseRecyclerAdapter<User, CustomerViewHolder>(
+                User.class,
+                R.layout.layout_customer_item,
+                CustomerViewHolder.class,
+                customersList.orderByChild("name").equalTo(text.toString())     //Compare name
+        ) {
+            @Override
+            protected void populateViewHolder(CustomerViewHolder viewHolder, User model, int position) {
+                viewHolder.txvName.setText(model.getName());
+                viewHolder.txvEmail.setText(model.getEmail());
+                viewHolder.txvPhone.setText(model.getPhone());
+                viewHolder.txvAddress.setText(model.getAddress());
+
+                final User local = model;
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(CustomersList.this, "" + local.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(searchAdapter);     //Set adapter for Recycler View is Search result
+    }
+
+    private void loadSuggest() {
+        customersList.orderByChild("Name")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot postSnapshot:dataSnapshot.getChildren())
+                        {
+                            User item = postSnapshot.getValue(User.class);
+                            suggestList.add(item.getName());        //Add name of customer/user
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void loadCustomersList() {
